313c56a05650cc87137be3d066731761c5336a6a
lokalinosps <natasaiivanjankovicii@gmail.com>
2026-03-19 17:42:29 +0100

    Dodana Google mapa na kontakt stranicu

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -12,9 +12,9 @@ const Contact = () => {
           FA für Innere Medizin, Kardiologie & Nephrologie, Anästhesiologie,
           Intensivmedizin, Schmerztherapie, Akupunktur
         </p>
-        <p>Alser Straße 20/7, 1090 Wien</p>
-        <p>Tel: +43 (0)1 336 13 36</p>
-        <p>Fax: DW 14</p>
+        <p>Laurenzgasse 9/GL1, 1050 Wien</p>
+        <p>Tel: +43 660 611 05 15</p>
+        
 
         <p>
           📧 <strong>Dr. Claudius Dörr:</strong>{' '}
@@ -38,7 +38,7 @@ const Contact = () => {
       <div style={{ marginTop: '2rem' }}>
         <iframe
           title="Ordination Dr. Dörr - Location"
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2653.048499936543!2d16.3559086!3d48.2197104!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x476d077383ba4b8f%3A0x8cbb3f3a20747bd7!2sAlser%20Str.%2020%2F7%2C%201090%20Wien%2C%20Austria!5e0!3m2!1sen!2srs!4v1716475958690!5m2!1sen!2srs"
+          src="https://www.google.com/maps?q=Laurenzgasse+9,+1050+Wien" target="_blank"
           width="100%"
           height="350"
           style={{ border: 0 }}
